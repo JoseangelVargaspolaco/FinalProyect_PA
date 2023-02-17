c8b63723ba1f83ec999be977e944ab4ede901c7e
JoseangelVargaspolaco <jose_vargas7@ucne.edu.do>
2023-02-17 12:41:13 -0400

    Commit inicial

---
+++ Migrations/20230217135431_Cuarta.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinal.Migrations
+{
+    public partial class Cuarta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventarios",
+                columns: table => new
+                {
+                    InventaeioId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    totalPrecio = table.Column<decimal>(type: "TEXT", nullable: false),
+                    totalCosto = table.Column<decimal>(type: "TEXT", nullable: false),
+                    totalCantidad = table.Column<double>(type: "REAL", nullable: false),
+                    EstaEliminado = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventarios", x => x.InventaeioId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventarios");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# FinalProyect_PA
-En este repositorio se aloja el proyecto de nuestro sistema de compra y ventas
+# Proyecto - Proyecto Aplicado
+# Sistema de Compra & Ventas
+
+ Descripción: Este es un sistema de Compra & Ventas, donde puedes registrar Suplidores, Clientes, Articulos, realizar ventas con detalle y tener control de inventario de tus Articulos. 
+        
+# Equipo desarrollador: Developers Jr
